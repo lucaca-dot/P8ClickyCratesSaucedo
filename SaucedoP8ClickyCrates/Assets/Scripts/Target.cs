8a46d4a17ddeea3b49c1822bd99a3e87ee245fe0
lucaca-dot <179270779+lucaca-dot@users.noreply.github.com>
2025-05-20 15:27:52 -0700

    I finished 3.1 and 3.2
    i have completed 3.1 and 3.2, and now just started 3.3

--- SaucedoP8ClickyCrates/Assets/Scripts/Target.cs
+++ SaucedoP8ClickyCrates/Assets/Scripts/Target.cs
@@ -11,6 +11,11 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -6;
 
+    private GameManager gameManager;
+    public ParticleSystem explosionParticle;
+
+    public int pointValue;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +25,9 @@ public class Target : MonoBehaviour
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
         transform.position = RandomSpawnPos();
+
+        gameManager = GameObject.Find("GameManager")
+            .GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -41,6 +49,20 @@ public class Target : MonoBehaviour
     Vector3 RandomSpawnPos()
     {
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position,
+        explosionParticle.transform.rotation);    
+        gameManager.UpdateScore(pointValue);
+    }
+
+    private void TriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 
 }
