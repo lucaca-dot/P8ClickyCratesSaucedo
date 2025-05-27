bb68b5b464df2dfb4c11a4e43df799b065010851
lucaca-dot <179270779+lucaca-dot@users.noreply.github.com>
2025-05-27 15:28:28 -0700

    I finished my game

--- SaucedoP8ClickyCrates/Assets/Scripts/GameManager.cs
+++ SaucedoP8ClickyCrates/Assets/Scripts/GameManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System.Runtime.CompilerServices;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Reflection;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,22 +14,23 @@ public class GameManager : MonoBehaviour
 
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI GameOverText;
+    public TextMeshProUGUI livesText;
+
+    public Button restartButton;
+    public GameObject titleScreen;
 
 
 
     private float spawnRate = 1.0f;
 
     public bool isGameActive;
-    
+
+    private int lives;
+
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
-
-        
+         
     }
 
     // Update is called once per frame
@@ -56,10 +60,40 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
+        restartButton.gameObject.SetActive(true);
         GameOverText.gameObject.SetActive(true);
         isGameActive = false;
     }
 
+    public void RestartGame()
+    {
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        score = 0;
+        spawnRate = spawnRate / difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        UpdateLives(3);
+
+        titleScreen.gameObject.SetActive(false);
+
+    }
+
+    public void UpdateLives(int livesToChange)
+    {
+        lives += livesToChange;
+        livesText.text = "Lives: " + lives;
+        if (lives <= 0)
+        { 
+            GameOver();
+        }
+    }
 
 }    
